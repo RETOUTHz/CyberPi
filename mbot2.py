3ebac372d0a5905e75b0d137220af9a864866e3a
Polllapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2024-06-14 20:15:08 +0700

    Add files via upload

---
+++ mbot2.py
@@ -0,0 +1,47 @@
+def forward(a:int,b:int):
+    """
+    """
+
+def straight(a:int):
+    """
+    """
+
+def turn(a:int):
+    """
+    """
+
+def EM_set_speed(a:int, b:str):
+    """
+    """
+
+def EM_stop(stop:str):
+    """
+
+    """
+def EM_turn(a:int,b:int,c:str):
+    """
+    """
+
+def drive_speed(a:int,b:int):
+    """
+    """
+
+def drive_power(a:int,b:int):
+    """
+    """
+
+def EM_stop():
+    """
+    """
+
+def EM_reset_angle():
+    """
+    """
+
+def EM_lock(a:int, b:str):
+    """
+    """
+    
+def set_auto():
+    """
+    """
