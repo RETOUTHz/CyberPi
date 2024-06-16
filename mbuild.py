046c665c87c91c226cbda0e4dc96862bb0e85320
Polllapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2024-06-16 19:40:28 +0700

    asd

---
+++ mbuild.py
@@ -0,0 +1,55 @@
+class quad_rgb_sensor():
+    def set_led_color(a:str,b:int):
+        """
+        """
+    
+    def close(a:int):
+        """
+        """
+    
+    def adjust(a:int):
+        """
+        """
+    
+    def set_custom_color(a:int,b:int,c:int,d:int):
+        """
+        """
+    
+    def color_mode():
+        """
+        """
+    
+    def get_line_sta():
+        """
+        """
+
+    def is_line():
+        """
+        """
+
+    def get_offset_track(a:int):
+        """
+        """
+
+class ultrasonic2():
+    def set_bri():
+        """
+        """
+
+    def add_bri():
+        """
+        """
+
+    def play():
+        """
+        """
+
+    def get(a:int):
+        """
+        """
+    
+    def get_br(a:int,b:int):
+        """
+        """
+
+
